29acab241559225966bef64b58e325e2afa4192f
PasinduGalwaththa <nawodyapasindu@gmail.com>
2023-07-15 18:30:45 +0530

    Delete set_arrivals directory

--- set_arrivals/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SetArrivalsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'set_arrivals'
--- set_arrivals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import SetArrivals
-
-class SetArrivalsSerializers(ModelSerializer):
-    class Meta:
-        model=SetArrivals
-        fields='__all__'
